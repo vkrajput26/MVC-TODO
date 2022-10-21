9c5494b90d96521290964ffbca70dbd6ce697234
Vivek Rajput <indvivekrajput@gmail.com>
2022-10-22 00:32:09 +0530

    adding post request

--- Routes/Controllers.route.js
+++ Routes/Controllers.route.js
@@ -8,6 +8,13 @@ mvctodoRouter.get("/", async(req,res)=>{
     res.send(data)
 })
 
+mvctodoRouter.post("/create", async(req,res)=>{
+    const payload=req.body;
+    await MvcModel.insertMany([payload])
+    res.send("data added successfully")
+})
+
+
 
 
 module.exports={mvctodoRouter}
